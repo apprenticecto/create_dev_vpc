a0ccdedd23873d586e3b5e602048f7475d4da61c
ApprenticeCTO <reachout@apprenticecto.com>
2020-11-14 11:13:43 +0100

    fixed outputs.tf sintax errors

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_public_subnets" {
   description = "IDs of the VPC's public subnets"
-  value       = module.vpc.public_subnets
+  value       = module.vpc_public_subnets
 }
 
 output "ec2_instance_public_ips" {
   description = "Public IP addresses of EC2 instances"
-  value       = module.ec2_instances.public_ip
+  value       = module.ec2_instances_public_ip
 }
